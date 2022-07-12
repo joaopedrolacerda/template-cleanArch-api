0a89c48e3901690733b4ab039a5f6b40a9fd0293
joao pedro <joaopedro.lacerda33@gmail.com>
2022-07-12 16:37:04 -0300

    change token to use 1 time

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -7,13 +7,13 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
   let sut: FacebookAuthenticationService
-
+  const token = 'any_token'
   beforeEach(() => {
     loadFacebookUserApi = mock()
     sut = new FacebookAuthenticationService(loadFacebookUserApi)
   })
   it('should cal LoadFacebookUserApi', async () => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
     expect(loadFacebookUserApi.loadUser).toBeCalledTimes(1)
@@ -21,7 +21,7 @@ describe('FacebookAuthenticationService', () => {
   it('should return AuthenticationError when loadFacebookUserApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
