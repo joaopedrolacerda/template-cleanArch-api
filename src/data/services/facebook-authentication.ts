07d3ad33a4a4d1ea29fdd3eed1376dd2fb3dfcc5
joao pedro <joaopedro.lacerda33@gmail.com>
2022-08-04 20:06:04 -0300

    refactor: simplify code using intersection types

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,16 +5,15 @@ import { FacebookAuthentication } from '@/domain/features'
 import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/respos'
 export class FacebookAuthenticationService {
   constructor (
-    private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi,
-    private readonly loadUserAccount: LoadUserAccountRepository,
-    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
+    private readonly facebookApi: LoadFacebookUserApi,
+    private readonly userAccountRepo: LoadUserAccountRepository & CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    const fbData = await this.loadFacebookUserByTokenApi.loadUser(params)
+    const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
-      await this.loadUserAccount.load({ email: fbData?.email })
-      await this.createFacebookAccountRepo.createFromFacebook(fbData)
+      await this.userAccountRepo.load({ email: fbData?.email })
+      await this.userAccountRepo.createFromFacebook(fbData)
     }
     return new AuthenticationError()
   }
